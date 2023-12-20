c687ab1c3b1b3623e376d3641caaac2bc432280e
Thanh25102 <manhthanh147@gmail.com>
2023-12-20 18:11:26 +0700

    Add Firebase settings and update sign-in view
    Added Firebase configuration settings for Android and iOS platforms to support Firebase services in the app. Created a new sign-in page for user authentication with third-party providers (Google, Facebook, and Apple) and phone number, along with the ability to switch to sign-up if an account exists already. Additionally, some color definitions in the app have been updated for further clarification.

--- lib/common/values/colors.dart
+++ lib/common/values/colors.dart
@@ -1,22 +1,23 @@
 import 'dart:ui';
 
 class AppColors {
-  /// 主背景 白色
+  /// bg white color
   static const Color primaryBackground = Color.fromARGB(255, 255, 255, 255);
-  /// 主背景 灰色
+  /// bg gray color
   static const Color primarySecondaryBackground = Color.fromARGB(255, 247, 247, 249);
-  /// 主控件-背景
+  /// primary color
   static const Color primaryElement = Color.fromARGB(255, 74, 123, 246);
-  /// 主文本 黑
+  /// primary text
   static const Color primaryText = Color.fromARGB(255, 32, 47, 62);
+
   static const Color primary_bg = Color.fromARGB(210, 32, 47, 62);
-  /// 主控件-文本 白色
+
   static const Color primaryElementText = Color.fromARGB(255, 255, 255, 255);
-  // 主控件-文本 灰色
+
   static const Color primarySecondaryElementText = Color.fromARGB(255, 194, 197, 201);
 
   static const Color primaryThreeElementText = Color.fromARGB(255, 99, 99, 99);
-  //状态
+
   static const Color primaryElementStatus = Color.fromARGB(255, 88, 174, 127);
 
   static const Color primaryElementBg = Color.fromARGB(255, 238, 121, 99);
